d55cbe87969fc66d809f0f6696b8032687331083
nelli04 <nelli_ivanova04@mail.ru>
2022-12-04 21:30:07 +0300

    b

---
+++ Repeat a String Repeat a String.js
@@ -0,0 +1,9 @@
+function repeatStringNumTimes(str, num) {
+    let string = '';
+    for (let i = 0; i < num; i++) {
+      string += str;
+    }
+    return string;
+  }
+  
+  repeatStringNumTimes("abc", 3);
